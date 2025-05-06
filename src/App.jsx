50993a4b4db5364e8f32ca9360e7a09269c08c02
yuta-yamaura <shk0fate@gmail.com>
2025-05-06 18:45:10 +0900

    FrontからBackendへAPI連携し、画像を表示させるところまで実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+
+  const TMDB_API_KEY = '5236c086a277777d7410fd8aa38d04d5'
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const {data} = await axios.get('http://127.0.0.1:8000/api/movies/693134/')
+      console.log(data)
+      setProducts(data)
+    }
+    fetchProducts();
+  }, [])
 
   return (
     <>
-      My Movie App
+      <div>
+          <img src={`https://image.tmdb.org/t/p/w500/${products.backdrop_path}`} alt="picture" />
+      </div>
     </>
   )
 }
